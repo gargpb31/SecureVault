563b34e5fb8faee6fcbee17f26800a3143c295c7
Sunny Garg <sunnygarg887@gmail.com>
2025-06-14 20:13:05 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import {
   ClerkProvider,
  
 } from "@clerk/nextjs";
-import { Geist, Geist_Mono } from "next/font/google";
+
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import { ThemeProvider } from "@/components/theme-provider";
